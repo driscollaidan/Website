c89bdbaae19652494be7458e83abf4414cebb2f4
driscollaidan <112516869+driscollaidan@users.noreply.github.com>
2022-11-13 16:58:30 -0500

    Delete sales_proj directory

--- sales_proj/graphing/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Sale(models.Model):
-    item = models.CharField(max_length=50)
-    price = models.FloatField()
-
-    def __str__(self):
-        return str(self.item)
